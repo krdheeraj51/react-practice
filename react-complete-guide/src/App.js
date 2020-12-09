46c56c7bfd16096003bff118fce13e63864c178f
krdheeraj51 <dheerajkumar@topsinfosoultions.com>
2020-12-09 08:38:35 +0530

    understanding state  management and adding style in side react component.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,37 +1,125 @@
-// import logo from './logo.svg';
-import React from 'react';
-import Person from './Person/person'
+// import React, { useState } from 'react';
+import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
+import { render } from '@testing-library/react';
+
+/**
+ * functional Component
+ */
+
+// const App = props => {
+//   const [ personState,setPersonState ] =
+//   useState({
+//       persons: [
+//         { name: 'Max', age: 28 },
+//         { name: 'Sntosh', age: 25 },
+//         { name: 'Manish', age: 24 }
+//       ],
+//       otherState:"this is another state"
+//     });
+
+//    const switchNameHandler = () =>{
+//       //console.log("was clicked ....");
+//       // this.state.persons[0].name="Maxima";
+//       setPersonState({
+//         persons:[
+//           {name:"Maxima",age:28},
+//           {name:"Manu",age:25},
+//           {name:"Manish",age:34}
+//         ]
+//       })
+//     };
+//     //console.log("personState ::",personState);
+
+//   return (
+//     <div className="App">
+//       <h1>Hi, I'm a React APP</h1>
+//       <p>This is really working.</p>
+//       {/* <Person name="Santosh" age="25"></Person>
+//           <Person name="Gulshan" age="24"></Person>
+//           <Person name="Karan" age="15">My hobbies are to play Kabddi and goes into deep meditation.</Person> */}
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <Person name={personState.persons[0].name} age={personState.persons[0].age} ></Person>
+//       <Person name={personState.persons[1].name} age={personState.persons[1].age} ></Person>
+//       <Person name={personState.persons[2].name} age={personState.persons[2].age} ></Person>
+//     </div>
+//   )
+
+// }
+// export default App;
 
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Hi, I'm a React APP</h1>
-    <p>This is really working.</p>
-    
+/**
+ * class component 
+ */
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Sntosh', age: 25 },
+      { name: 'Manish', age: 24 }
+    ]
+  }
+
+  render() {
+    const style ={
+      backgroundColor:'white',
+      font:'inherit',
+      border:'1px solid blue',
+      padding:'8px',
+      cursor:'pointer'
+    };
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React APP</h1>
+        <p>This is really working.</p>
+        {/* <Person name="Santosh" age="25"></Person>
+        <Person name="Gulshan" age="24"></Person>
+        <Person name="Karan" age="15">My hobbies are to play Kabddi and goes into deep meditation.</Person> */}
+        <button 
+         style={style} 
+        onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
+      </div>
+
+    )
+  }
+  switchNameHandler = () => {
+    console.log("was clicked ....");
+    // this.state.persons[0].name="Maxima";
+    this.setState({
+      persons: [
+        { name: "Maxima", age: 28 },
+        { name: "Manu", age: 25 },
+        { name: "Manish", age: 34 }
+      ]
+    })
+  }
 
 
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Person name="Santosh" age="25"></Person>
-      <Person name="Gulshan" age="24"></Person>
-      <Person name="Karan" age="15">My hobbies are to play Kabddi and goes into deep meditation.</Person>
-    </div>
-  );
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//     <h1>Hi, I'm a React APP</h1>
+//     <p>This is really working.</p>
+//       <Person name="Santosh" age="25"></Person>
+//       <Person name="Gulshan" age="24"></Person>
+//       <Person name="Karan" age="15">My hobbies are to play Kabddi and goes into deep meditation.</Person>
+//     </div>
+//   );
+// }
+
+
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,13 +1,15 @@
 import React from 'react';
-const Person = (props) => {
+import './Person.css';
+const person = (props) => {
     // return <p>I'm a Person!!!!</p>
     return (
-        <div>
+        <div className="Person">
             <p>I am {props.name}. I am {props.age} years old.</p>
             <p>I like to code and create open source platform.</p>
             <p>{props.children}</p>
+            <input type="text" />
         </div>
 
     )
 }
-export default Person;
+export default person;
